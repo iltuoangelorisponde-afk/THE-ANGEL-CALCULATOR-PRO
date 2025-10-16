c6f9326e3ccdd6d44d1a1e77e3f096431502f525
iltuoangelorisponde-afk <iltuoangelorisponde@gmail.com>
2025-10-16 15:48:21 +0200

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AngelCalculator from './AngelCalculatorPro';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AngelCalculator />
+  </React.StrictMode>
+);
