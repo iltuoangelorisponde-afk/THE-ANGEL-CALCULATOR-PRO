f26333ba17a4f9d1b8ea46d5843cdb795d944976
iltuoangelorisponde-afk <iltuoangelorisponde@gmail.com>
2025-10-16 15:49:00 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AngelCalculatorPro from './AngelCalculatorPro';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AngelCalculatorPro />);
